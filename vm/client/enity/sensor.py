bf190cc970e0985e7190dfaa1b8c14ce78a1efb9
Liudmi1a <klim.cspr@gmail.com>
2025-05-29 21:56:53 +0300

    Update sensor.py

--- vm/client/enity/sensor.py
+++ vm/client/enity/sensor.py
@@ -1,21 +1,14 @@
-import paho.mqtt.client as paho
-from os import environ
-import time
-from entity.sensor import *
-broker = "localhost" if "SIM_HOST" not in environ.keys() else environ["SIM_HOST"]
-port = 1883 if "SIM_PORT" not in environ.keys() else environ["SIM_PORT"]
-name = "sensor" if "SIM_NAME" not in environ.keys() else environ["SIM_NAME"]
-period = 1 if "SIM_PERIOD" not in environ.keys() else int(environ["SIM_PERIOD"])
-type_sim = "temperature" if "SIM_TYPE" not in environ.keys() else environ["SIM_TYPE"]
-sensors = {"temperature": Temperature, "pressure": Pressure, "current": Current, "carbon_oxid": CO}
-def on_publish(client, userdata, result):  # create function for callback
-    print(f"data published {userdata}")
-    pass
-sensor = sensors[type_sim](name=name)
-client1 = paho.Client(sensor.name)  # create client object
-client1.on_publish = on_publish  # assign function to callback
-client1.connect(broker, port)  # establish connection
-while True:
-    sensor.generate_new_value()
-    ret = client1.publish("sensors/" + sensor.type + "/" + sensor.name, sensor.get_data())  # publish
-    time.sleep(period)
+ import math
+        self.value = math.sin(self.step)
+        self.step = self.step + 1
+
+class CO(Sensor):
+    step = 0
+
+    def __init__(self,name):
+        super().__init__(name)
+        self.type = "carbon oxid"
+
+    def generate_new_value(self):
+        self.value = self.step * 1e6
+        self.step = self.step + 0.001
